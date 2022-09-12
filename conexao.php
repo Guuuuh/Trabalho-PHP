ec9d0690a11ea21767398b7fd852cd9af069471b
Guuuuh <gustavo.htl@hotmail.com>
2022-09-12 10:53:29 -0300

    conexao com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+    $endereco = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $base = "trabalho_php";
+
+    $conexao = mysqli_connect($descricao, $marca, $estoque, $preco);
+?>
