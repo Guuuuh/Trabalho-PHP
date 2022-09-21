6e7946136a78d3294832b146eaa95259882f6a49
Guuuuh <gustavo.htl@hotmail.com>
2022-09-20 21:00:53 -0300

    Estilizando o trabalho
    mensagem ao atualizar algum produto, e botão para volta ao select

--- editar.php
+++ editar.php
@@ -3,7 +3,7 @@
 
     require('conexao.php');
     $resultado = mysqli_query($conexao, "SELECT * FROM produtos WHERE id = $id");
-    $usuario = mysqli_fetch_assoc($resultado); 
+    $produto = mysqli_fetch_assoc($resultado); 
 ?>
 <!doctype html>
 <html lang="en">
@@ -11,6 +11,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Editar produtos</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </head>
@@ -18,22 +19,22 @@
     <div class="card m-4 bg-secondary"> 
         <div class="card-body">
             <form action="update.php" method="post">
-                <input type="hidden" name="id" value="<?=$usuario['id']?>">
+                <input type="hidden" name="id" value="<?=$produto['id']?>">
                 <div class="mb-3">
                     <label for="descricao" class="form-label">Descrição:</label>
-                    <input type="text" class="form-control" id="descricao" name="descricao" value="<?=$usuario['descricao']?>">
+                    <input type="text" class="form-control" id="descricao" name="descricao" value="<?=$produto['descricao']?>">
                 </div>
                 <div class="mb-3">
                     <label for="marca" class="form-label">Marca:</label>
-                    <input type="text" class="form-control" id="marca" name="marca" value="<?=$usuario['marca']?>">
+                    <input type="text" class="form-control" id="marca" name="marca" value="<?=$produto['marca']?>">
                 </div>
                 <div class="mb-3">
                     <label for="estoque" class="form-label">Estoque:</label>
-                    <input type="number" class="form-control" id="estoque" name="estoque" value="<?=$usuario['estoque']?>">
+                    <input type="number" class="form-control" id="estoque" name="estoque" value="<?=$produto['estoque']?>">
                 </div>
                 <div class="mb-3">
                     <label for="preco" class="form-label">Preço:</label>
-                    <input type="number" class="form-control" id="preco" name="preco" value="<?=$usuario['preco']?>">
+                    <input type="number" class="form-control" id="preco" name="preco" value="<?=$produto['preco']?>">
                 </div>
                 <div class="mb-3 ">
                     <input type="submit" class="form-control w-25 bg-primary" value="Atualizar" >
--- select.php
+++ select.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Selecionar produtos</title>
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   </head>
   <body>
-    <div class="card m-4"> 
+    <div class="card m-4 cor-background" > 
       <div class="card-body">
         <table class="table table-dark table-striped">
           <tr>
